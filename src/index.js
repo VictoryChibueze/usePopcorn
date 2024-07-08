9664935582617bd99e3f536b9feec68c5c2a6270
VictoryChibueze <alhajivicci12@gmail.com>
2024-07-08 15:41:31 +0100

    starRating

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      messages={["Terribe", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating maxRating={10} color="#12ab00" size={26} defaultRating={3} />
   </React.StrictMode>
 );
